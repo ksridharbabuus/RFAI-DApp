fa70b0ccee1741625b4e0be7c8f0fe6d32635420
Sridhar Babu Kolapalli <ksridharbabuus@gmail.com>
2019-02-10 23:31:56 +0530

    Tabs visibility based on roles

--- src/layouts/home/components/RequestsTab/index.js
+++ src/layouts/home/components/RequestsTab/index.js
@@ -31,44 +31,89 @@ class RequestsTab extends Component {
     this.contracts = context.drizzle.contracts;
 
     this.state = {
-      selectedTab: 0,
+      dataKeyMemberKeys: null,
+      foundationMembers: [],
+      dataKeyOwner: null,
+      owner: null,
+      selectedTab: 1,
       dialogOpen: false,
+      isFoundationMember: false,
       alertText: ''
     }
 
   }
 
   componentDidMount() {
+    const dataKeyMemberKeys = this.contracts.ServiceRequest.methods.getFoundationMemberKeys.cacheCall();
+    this.setState({dataKeyMemberKeys})
+    this.setFoundationMembers(this.props.ServiceRequest)
+
+    const dataKeyOwner  = this.contracts.ServiceRequest.methods.owner.cacheCall();
+    this.setState({dataKeyOwner})
+    this.setOwner(this.props.ServiceRequest)
 
   }
 
-  componentDidUpdate(prevProps) {
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.ServiceRequest !== prevProps.ServiceRequest || this.state.dataKeyMemberKeys !== prevState.dataKeyMemberKeys) {
+        this.setFoundationMembers(this.props.ServiceRequest)
+    }
+    if (this.props.ServiceRequest !== prevProps.ServiceRequest || this.state.dataKeyOwner !== prevState.dataKeyOwner) {
+      this.setOwner(this.props.ServiceRequest)
+    }
+  }
+
+  setFoundationMembers(contract) {
+
+    if (contract.getFoundationMemberKeys[this.state.dataKeyMemberKeys] !== undefined && this.state.dataKeyMemberKeys !== null) {
+      this.setState({
+        foundationMembers: contract.getFoundationMemberKeys[this.state.dataKeyMemberKeys].value
+      }, () => {
+        const exists = this.state.foundationMembers.some(m => m === this.props.accounts[0])
+        if(exists) {
+          this.setState({isFoundationMember : exists});
+          //if(this.state.selectedTab !== 0) this.setState ({selectedTab : 0})  
+        }
+          
+      });
+    }
 
   }
 
+  setOwner(contract) {
+    if (contract.owner[this.state.dataKeyOwner] !== undefined && this.state.dataKeyOwner !== null) {
+      this.setState({
+        owner: contract.owner[this.state.dataKeyOwner].value
+      }, () => {
+        if( this.state.owner === this.props.accounts[0]) {
+          this.setState({isFoundationMember : true});
+          //if(this.state.selectedTab !== 0) this.setState ({selectedTab : 0})
+        }          
+      });
+    }
+  }
 
   handleChange = (event, value) => {
     this.setState({ selectedTab: value });
-    console.log("selectedTab - " + value);
+console.log("selectedTab - " + value);
   };
 
   render() {
 
     const selectedTab = this.state.selectedTab;
-    
     return (
       <div className="main-content">
       <div className="main">
         <AppBar position="static" color="default">
           <Tabs value={selectedTab} onChange={this.handleChange}>
-            <Tab label="Open " />
-            <Tab label="Approved " />
-            <Tab label="Rejected " />
-            <Tab label="Closed " />
-            <Tab label="Expired " />
+            {this.state.isFoundationMember === true && <Tab label="Open " value={0}/> }
+            <Tab label="Approved " value={1}/>
+            <Tab label="Rejected " value={2} />
+            <Tab label="Closed " value={3} />
+            <Tab label="Expired " value={4} />
           </Tabs>
         </AppBar>
-        {selectedTab === 0 && <Typography component="div" ><RequestListV2  compRequestStatus="0"/> </Typography>}
+        {selectedTab === 0 && this.state.isFoundationMember === true && <Typography component="div" ><RequestListV2  compRequestStatus="0"/> </Typography>}        
         {selectedTab === 1 && <Typography component="div" ><RequestListV2  compRequestStatus="1"/> </Typography>}
         {selectedTab === 2 && <Typography component="div" ><RequestListV2  compRequestStatus="2"/> </Typography>}
         {selectedTab === 3 && <Typography component="div" ><RequestListV2  compRequestStatus="4"/> </Typography>}
